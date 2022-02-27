6a6a93cad6fa9972010f345720842dfe07db7e71
hardyrafael17 <hardy.rafael17@gmail.com>
2022-02-27 19:33:45 +0100

    initial update

---
+++ get_next_line.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hjimenez <hjimenez@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/29 08:22:33 by hjimenez          #+#    #+#             */
+/*   Updated: 2022/02/27 19:17:56 by hjimenez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef GET_NEXT_LINE
+# define GET_NEXT_LINE
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct fd_data_list
+{
+	int			fd;
+	char		*left_over;
+	int			eof;
+	int			return_null;
+}				t_fd;		
+
+size_t			ft_check_nl(char *read_buffer, size_t size);
+size_t			ft_strlcat(char *dst, const char *src, size_t size);
+size_t			ft_strlcpy(char *dst, const char *src, size_t size);
+char			*ft_substr(char const *s, unsigned int start, size_t len);
+size_t			ft_strlen(const char *str);
+char			*ft_strjoin(char const *s1, char const *s2);
+void			*ft_calloc(size_t count, size_t size);
+char			*get_next_line(int fd);
+int				ft_append(char *to_append, t_fd *buffer_to_update);
+char			*ft_return_line(t_fd *buffer_to_update);
+void			ft_read(t_fd *t_fd_data, int fd);
+size_t 			ft_strlcpy_strlen(char *dst, const char *src, size_t size);
+
+#endif
